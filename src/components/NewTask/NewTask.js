9526d4d07167edba28fcb6d487a071eecfd6d378
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-18 14:37:28 +0200

    refactor with useHttp custom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,30 @@
-import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: 'https://http-custom-hooks-1ad78-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      createTask.bind(null, taskText) // since createTask needs tasKText and doesn't have access elsewhere, unless we defined createTask inside enterTaskHandler, but that would be a second level of nesting which is a bit much. When createTask is called in useHttp hook with data as only argument, this data argument is appended to end of list, thereby being the second argument since we are configuring the first one here
+    );
   };
 
   return (
